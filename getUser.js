bbb60ace16962ed7ae3f0e3923f02bc9fa066b0d
Abdul Arham <abdul.arham9123@gmail.com>
2024-01-29 23:44:42 +0500

    updated getUser.js

--- getUser.js
+++ getUser.js
@@ -4,6 +4,3 @@ export const getUser = (userToken)=>{
     const string = verify(userToken, process.env.JWT_SECRET)
     return string.user;
 }
-
-// const user = getUser("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoyfSwiaWF0IjoxNzA2NTM2NjY4fQ.H8_Y2WBA3hZFRQXg6LK-4o6Y-oLHTYSoDgstaoZ7QVM")
-// console.log(user);
